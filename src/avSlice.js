1d45ed581acabce5e572e39b92319ab831105d76
yuyang <ongyuyang2505@gmail.com>
2024-10-03 09:08:59 -0400

    completed

--- src/avSlice.js
+++ src/avSlice.js
@@ -3,16 +3,53 @@ import { createSlice } from "@reduxjs/toolkit";
 export const avSlice = createSlice({
   name: "av",
   initialState: [
-    
+        {
+        img: "",
+        name: "Projectors",
+        cost: 200,
+        quantity: 0,
+    },
+    {
+        img: "",
+        name: "Speaker",
+        cost: 35,
+        quantity: 0,
+    },
+    {
+        img: "",
+        name: "Microphones",
+        cost: 45,
+        quantity: 0,
+    },
+    {
+        img: "",
+        name: "Whiteboards",
+        cost: 80,
+        quantity: 0,
+    },
+
+    {
+        img: "",
+        name: "Signage",
+        cost: 80,
+        quantity: 0,
+    },
+
   ],
 
 
   reducers: {
     incrementAvQuantity: (state, action) => {
-      
+        const item = state[action.payload];
+        if (item) {
+            item.quantity++;
+        }
     },
     decrementAvQuantity: (state, action) => {
-     
+        const item = state[action.payload];
+        if (item && item.quantity > 0) {
+            item.quantity--;
+        }
     },
   },
 });
